89bc61cbe18a561180c521fc4074c95f04b91865
Joel Azemar <joel.azemar@gmail.com>
2021-02-04 18:55:48 +0100

    Update Tab2.tsx

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -4,11 +4,20 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon
 } from '@ionic/react';
+import { camera } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+
+  const takePhoto = () => {
+    console.log('takePhoto');
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,13 +25,12 @@ const Tab2: React.FC = () => {
           <IonTitle>Tab 2</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse='condense'>
-          <IonToolbar>
-            <IonTitle size='large'>Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name='Tab 2 page' />
+      <IonContent>
+        <IonFab vertical='bottom' horizontal='center' slot='fixed'>
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
